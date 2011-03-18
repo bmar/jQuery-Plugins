2cf18e72483fb2c0bdc4230570447391ddbe4a2c
bmar <bradmar@gmail.com>
2011-03-17 17:43:16 -0700

    converted the plugin style; added 'config' object with 'inverse' boolean; added 'defaults' object. 'getSortable' is not in 'config' -- still 2nd parameter because it just feels better, k?
    Signed-off-by: bmar <bradmar@gmail.com>

--- sortElements/jquery.sortElements.js
+++ sortElements/jquery.sortElements.js
@@ -1,69 +1,75 @@
-/**
- * jQuery.fn.sortElements
- * --------------
- * @author James Padolsey (http://james.padolsey.com)
- * @version 0.11
- * @updated 18-MAR-2010
- * --------------
- * @param Function comparator:
- *   Exactly the same behaviour as [1,2,3].sort(comparator)
- *   
- * @param Function getSortable
- *   A function that should return the element that is
- *   to be sorted. The comparator will run on the
- *   current collection, but you may want the actual
- *   resulting sort to occur on a parent or another
- *   associated element.
- *   
- *   E.g. $('td').sortElements(comparator, function(){
- *      return this.parentNode; 
- *   })
- *   
- *   The <td>'s parent (<tr>) will be sorted instead
- *   of the <td> itself.
- */
-jQuery.fn.sortElements = (function(){
-    
-    var sort = [].sort;
-    
-    return function(comparator, getSortable) {
-        
-        getSortable = getSortable || function(){return this;};
-        
-        var placements = this.map(function(){
-            
-            var sortElement = getSortable.call(this),
-                parentNode = sortElement.parentNode,
-                
-                // Since the element itself will change position, we have
-                // to have some way of storing it's original position in
-                // the DOM. The easiest way is to have a 'flag' node:
-                nextSibling = parentNode.insertBefore(
-                    document.createTextNode(''),
-                    sortElement.nextSibling
-                );
-            
-            return function() {
-                
-                if (parentNode === this) {
-                    throw new Error(
-                        "You can't sort elements if any one is a descendant of another."
-                    );
-                }
-                
-                // Insert before flag:
-                parentNode.insertBefore(this, nextSibling);
-                // Remove flag:
-                parentNode.removeChild(nextSibling);
-                
-            };
-            
-        });
-       
-        return sort.call(this, comparator).each(function(i){
-            placements[i].call(getSortable.call(this));
-        });
-        
-    };
-    
-})();
+(function(jQuery) {
+    jQuery.extend(true, jQuery.fn, {
+		/**
+		 * jQuery.fn.sortElements
+		 * --------------
+		 * @param Function comparator:
+		 *   Exactly the same behaviour as [1,2,3].sort(comparator)
+		 *   
+		 * @param Function getSortable
+		 *   A function that should return the element that is
+		 *   to be sorted. The comparator will run on the
+		 *   current collection, but you may want the actual
+		 *   resulting sort to occur on a parent or another
+		 *   associated element.
+		 *   
+		 *   E.g. $('td').sortElements({'comparator':"myComparator"}, function(){
+		 *      return this.parentNode; 
+		 *   })
+		 *   
+		 *   The <td>'s parent (<tr>) will be sorted instead
+		 *   of the <td> itself.
+		 */
+		sortElements: function(config,getSortable){
+			var defaults = {
+				"default" : function(a, b){
+						return jQuery(a).text() > jQuery(b).text() ? 1 : -1;
+					},
+				"numeric" : function(a, b){
+						return parseInt(jQuery(a).text(), 10) > parseInt(jQuery(b).text(), 10) ? 1 : -1;
+					},
+				"auto" : function(a, b){
+					a = jQuery(a).text();
+					b = jQuery(b).text();
+				
+					return (
+						isNaN(a) || isNaN(b) ?
+						a > b : +a > +b
+					) ?
+						inverse ? -1 : 1 :
+						inverse ? 1 : -1;
+					}
+			}
+			config = config || {};
+			var inverse = config.inverse || false;
+			var sort = [].sort;
+			var comparator = defaults[config.comparator] || config.comparator || defaults['default'];
+			var getSortable = getSortable || function(){return this;};
+			var placements = this.map(function(){
+				var sortElement = getSortable.call(this),
+					parentNode = sortElement.parentNode,
+					// Since the element itself will change position, we have
+					// to have some way of storing its original position in
+					// the DOM. The easiest way is to have a 'flag' node:
+					nextSibling = parentNode.insertBefore(
+						document.createTextNode(''),
+						sortElement.nextSibling
+					);
+				return function() {
+					if (parentNode === this) {
+						throw new Error(
+							"You can't sort elements if any one is a descendant of another."
+						);
+					}
+					// Insert before flag:
+					parentNode.insertBefore(this, nextSibling);
+					// Remove flag:
+					parentNode.removeChild(nextSibling);
+				};
+			});
+			return sort.call(this, comparator).each(function(i){
+				placements[i].call(getSortable.call(this));
+			})
+		}
+    });
+})(jQuery);
